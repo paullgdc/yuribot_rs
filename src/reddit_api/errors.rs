4f07b4bc15969fba7f28dcb8dbe8333ba92fa78c
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2019-02-24 23:48:37 +0100

    updated error dipslay

--- src/reddit_api/errors.rs
+++ src/reddit_api/errors.rs
@@ -2,12 +2,12 @@ use failure::Fail;
 
 #[derive(Debug, Fail)]
 pub enum RedditError {
-    #[fail(display = "network error while fetching")]
+    #[fail(display = "network error while fetching from reddit")]
     NetworkError,
-    #[fail(display = "error while parsing response")]
+    #[fail(display = "error while parsing response from reddit api call")]
     ParsingError,
-    #[fail(display = "reddit Api returned a {} code", error_code)]
+    #[fail(display = "reddit api returned a {} code", error_code)]
     ApiError {error_code : u16 },
-    #[fail(display = "received unexpected result from api call")]
+    #[fail(display = "received unexpected result from reddit api call")]
     UnexpectedResponse,
 }
