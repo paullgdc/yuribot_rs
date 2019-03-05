aea2fb70028139653efe925668016ff495aaa87d
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2019-03-05 19:10:12 +0100

    use #[allow(dead_code)] on reddit api parameters constant

--- src/reddit_api/mod.rs
+++ src/reddit_api/mod.rs
@@ -161,11 +161,13 @@ pub struct Link {
 #[derive(Debug)]
 pub struct Sort(&'static str);
 
+#[allow(dead_code)]
 impl Sort {
     pub const NEW: Sort = Sort("/new");
     pub const BEST: Sort = Sort("/best");
     pub const TOP: Sort = Sort("/top");
     pub const CONTROVERSIAL: Sort = Sort("/controversial");
+    pub const HOT: Sort = Sort("/hot");
 
     fn as_str(&self) -> &'static str {
         self.0
@@ -175,6 +177,7 @@ impl Sort {
 #[derive(Debug)]
 pub struct MaxTime(&'static str);
 
+#[allow(dead_code)]
 impl MaxTime {
     pub const ALL: MaxTime = MaxTime("all");
     pub const YEAR: MaxTime = MaxTime("year");
